c1b34300ace376515f89fece4e0f9e600edd8e84
jeffrey burke <burkejeffrey56@yahoo.com>
2022-10-29 21:00:47 -0400

    Having trouble with the src atttribute functions

--- script.js
+++ script.js
@@ -1,11 +1,52 @@
 // variables
-let result = document.getElementById('result');
-let recipeBtn = document.getElementById('recipe-btn');
+let title = document.getElementById('title')
+let changeBtn = document.getElementById('change-btn');
+let recipePic = document.getElementById('recipe-pic');
 let youtubeBtn = document.getElementById('youtube-btn');
-let url = "https://www.themealdb.com/api/json/v1/1/randomselection.php";
+let ingredients = document.getElementById('ingredients');
+let instructions = document.getElementById("instructions")
+let url = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-  axios
-    .get(url )
-    .then(response => {
-      console.log(response)
-    })
+// functions to change recipes
+  function newRecipe() {
+//  get request function to get info from the meal api
+    axios.get(url)
+     .then(response => {
+       const myMeal = response.data.meals[0]
+       setTitle(myMeal);
+       setRecipePic(myMeal);
+       setInstructions(myMeal); 
+       setYoutube(myMeal); 
+       setIngredients(myMeal);
+    })  
+    
+    function setTitle(recipe) {
+      document.getElementById('title').innerHTML = recipe.strMeal; 
+    }
+
+    function setRecipePic(pic) {
+      console.log(pic.strMealThumb)
+      document.getElementById('recipePic').src = pic.strMealThumb;
+    }
+
+    function setInstructions(instructions) {
+      document.getElementById("instructions").innerHTML = instructions.strInstructions;
+    }
+
+    function setYoutube(video) {
+      document.getElementById('youtubeBtn').src = video.strYoutube
+    } 
+
+    function setIngredients(list) {
+      let  theIngredients = list.strIngredients
+      console.log(theIngredients)
+        for (let i = 0 ; i < theIngredients.length; i++) {
+          if (theIngredients[i] !== "") {
+
+
+      }
+
+    } document.getElementById("ingredients").innerHTML  = 
+  }
+}
+     
