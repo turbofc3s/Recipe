11c358033e483800d58326a7b5a65d5ee5778c03
jeffrey burke <burkejeffrey56@yahoo.com>
2022-11-06 21:53:49 -0500

    trying to get onload to work

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
-// variables
+
+
+ window.addEventListener('load',  function() { 
+    
+  
+    // variables
 let title = document.getElementById('title')
 let changeBtn = document.getElementById('change-btn');
 let recipePic = document.getElementById('recipe-pic');
 let youtubeBtn = document.getElementById('youtube-btn');
-let ingredients = document.getElementById('ingredients');
+let list = document.getElementById('list');
 let instructions = document.getElementById("instructions")
 let url = "https://www.themealdb.com/api/json/v1/1/random.php";
 
-  function newRecipe() {
+  changeBtn.addEventListener('click', function() {
 //  get request function to get info from the meal api
     axios.get(url)
      .then(response => {
@@ -17,7 +22,10 @@ let url = "https://www.themealdb.com/api/json/v1/1/random.php";
        setInstructions(myMeal.strInstructions);
        if(myMeal.strYoutube) {
         setVideo(myMeal.strYoutube);
-       } 
+       } else {
+        youtubeBtn.remove
+        console.log(myMeal.s)
+       }
        setIngredients(myMeal);
      })
     
@@ -46,8 +54,17 @@ let url = "https://www.themealdb.com/api/json/v1/1/random.php";
         ingredients.push(`${measure} ${ingredient}`);
       }
      }
-       document.getElementById('ingredients').innerHTML = ingredients;
-    //   let count = 1
+       console.log(ingredients)
+
+       ingredients.forEach((item) => {
+        let li = document.createElement('li');
+        li.innerText = item;
+        list.appendChild(li);
+      });
+
+    //    }
+    //    document.getElementById('ingredients').innerHTML = ingredients;
+    // //   let count = 1
     //   let mealKeys =  Object.keys(meal)
     //   let mealValues = Object.values(meal)
     //   console.log(mealKeys + mealValues)
@@ -74,8 +91,10 @@ let url = "https://www.themealdb.com/api/json/v1/1/random.php";
       let youTubeUrl = 'https://www.youtube.com/embed/' + id;
       document.getElementById('youtube-btn').src = youTubeUrl;
       // document.getElementById('youtube-btn').src = vid.strYoutube;
-    }
+    }      
+})
+  
 
-      
-}
-     
+  })
+
+  
